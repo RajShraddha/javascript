2755cd7fef953eb75ab1b21168afc0585c426d80
RajShraddha <shraddharaj1995@gmail.com>
2023-05-01 17:34:02 +0530

    'calendar'

---
+++ calendar.js
@@ -0,0 +1,82 @@
+
+const DAYS = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY']
+const MONTHS = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER']
+
+function displayCalendar() {
+    const calendar = document.getElementById('calendarBox')
+    calendar.style.display = "block"
+}
+
+function selectedMonth(){
+    const dropdown = document.getElementById('dropdown')
+    const selectedOption = dropdown.options[dropdown.selectedIndex]
+    return selectedOption.value
+}
+// let weekRow = document.createElement('tr')
+// weekRow.id = 'weekRow'
+function displayDates(){
+    deleteCurrentCalendarRow();
+    
+    const todayDate = new Date ()
+    const thisYear = todayDate.getFullYear()
+    const month = selectedMonth();
+    const thisMonth = MONTHS.indexOf(month.toUpperCase())
+    const noOfDays = new Date(thisYear, thisMonth+1, 0).getDate();
+    const firstDay = new Date(thisYear, thisMonth, 1).getDay();
+
+    weekRow = document.createElement('tr');
+    weekRow.id = 'weekRow'
+    // add first empty cells for the first week when required
+    for(i = 0; i < firstDay; i++){
+        const dayCell = document.createElement('td');
+        dayCell.textContent = ' '
+        dayCell.style.padding = '15px'
+        dayCell.style.backgroundColor = 'EEAD56'
+        weekRow.appendChild(dayCell)
+    }
+
+    // add month's first week dates
+    let date = 1;
+    for(i = firstDay; i <= 6; i++){
+        const dayCell = document.createElement('td');
+        dayCell.textContent = date
+        dayCell.style.padding = '15px'
+        dayCell.style.backgroundColor = 'EEAD56'
+        weekRow.appendChild(dayCell)
+        date++;
+    }
+    calendarRows.appendChild(weekRow)
+    
+    // add rest of the weeks
+    while(date <= noOfDays){
+        weekRow = document.createElement('tr')
+        weekRow.id = 'weekRow'
+
+        for(i = 0; i <=6; i++){
+            const dayCell = document.createElement('td')
+            dayCell.textContent = date;
+            dayCell.style.padding = '15px';
+            dayCell.style.backgroundColor = 'EEAD56'
+            weekRow.appendChild(dayCell)
+            date++;
+            if (date > noOfDays){          //break if date exceeds no of days of the month
+                break;
+            }
+        }
+        calendarRows.appendChild(weekRow)
+    }
+}
+
+// const createCalendarRows = ()=>{
+//     deleteCurrentCalendarRow();
+
+//     return document.createElement('tr');
+// }
+function deleteCurrentCalendarRow(){
+    let calendarRows = document.getElementById('calendarRows')
+    let weekNodes = calendarRows.querySelectorAll('#weekRow')
+    console.log(weekNodes);
+    // for(week in weekNodes){
+    //     calendarRows.children[0].remove()
+    // }
+}
